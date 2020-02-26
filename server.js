6006dfeaa8d5348e358f317c6f71531039b160e5
Wiesława Bryzik <wieslawa.bryzik@gmail.com>
2020-02-26 23:47:19 +0100

    Add structure, views for client & develop server

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
 const express = require('express');
 const app = express();
-// const socket = require('socket.io');
+const socket = require('socket.io');
 
+const tasks = [];
 
 app.use((req, res) => {
     res.status(404).send({ message: 'Not found...' });
-  })
-  
-  const server = app.listen(8000, () => {
+})
+
+const server = app.listen(8000, () => {
     console.log('Server is running on Port:', 8000)
-  });
-//   const io = socket(server);
+});
+const io = socket(server);
+
+
+
+io.on('connection', socket => {
+    socket.emit('updateData', tasks);
+
+
+    socket.on('addTask', newTask => {
+        tasks.push(newTask);
+        socket.broadcast.emit('addTask', newTask)
+    });
+
+    socket.on('removeTask', () => {
+        for (let outTask of tasks) {
+            if (outTask.task === socket.task) {
+
+                tasks.splice(tasks.indexOf(outTask), 1);
+
+                socket.broadcast.emit('removeTask', tasks)
+            }
+        }
+    });
+});
